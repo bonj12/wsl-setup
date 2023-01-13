d0a18e625fc7fe9f550f8209271f9c3e692aaa81
Ronald Barry Bonje <32478404+bonj12@users.noreply.github.com>
2023-01-13 12:01:45 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo chmod 777 -R ~/wsl-setup/
-mv ~/wsl-setup/focrex-build.mk ~/focrex-build.mk
-mv ~/wsl-setup/bxfg-build.mk ~/bxfg-build.mk
-mv ~/wsl-setup/alias-setup.sh ~/alias-setup.sh
-sudo rm -rf ~/wsl-setup
+mv ~/wsl-setup/focrex-build.mk ../focrex-build.mk
+mv ~/wsl-setup/bxfg-build.mk ../bxfg-build.mk
+mv ~/wsl-setup/alias-setup.sh ../alias-setup.sh
+sudo rm -rf wsl-setup
